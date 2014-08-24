85266e5f9197d05d22ffdac284d7dc4c622d7533
Patrik Johansson <patrikjohansson@Patriks-MacBook-Pro.local>
2014-08-24 22:18:13 +0200

    Added class

---
+++ PJTest.h
@@ -0,0 +1,13 @@
+//
+//  PJTest.h
+//  Git Test
+//
+//  Created by Patrik Johansson on 24/08/14.
+//  Copyright (c) 2014 PJ Games. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PJTest : NSObject
+
+@end
---
+++ PJTest.m
@@ -0,0 +1,13 @@
+//
+//  PJTest.m
+//  Git Test
+//
+//  Created by Patrik Johansson on 24/08/14.
+//  Copyright (c) 2014 PJ Games. All rights reserved.
+//
+
+#import "PJTest.h"
+
+@implementation PJTest
+
+@end
